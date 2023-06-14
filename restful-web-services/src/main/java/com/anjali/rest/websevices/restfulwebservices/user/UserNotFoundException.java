362a40f7287f46e387e0dcb5fc0d979a76e49c12
Anjali Rana <anjalirana7870@gmail.com>
2023-06-14 22:55:38 +0530

    exeptional handling

---
+++ restful-web-services/src/main/java/com/anjali/rest/websevices/restfulwebservices/user/UserNotFoundException.java
@@ -0,0 +1,11 @@
+package com.anjali.rest.websevices.restfulwebservices.user;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.NOT_FOUND)
+public class UserNotFoundException extends RuntimeException {
+    public UserNotFoundException(String mesage) {
+        super(mesage);
+    }
+}
